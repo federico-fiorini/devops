42c26c0a957586871f0840a4ef2dcd7e3b1151de
Federico Fiorini <federico.fiorini@hubchat.com>
2017-03-24 16:43:15 +0100

    detach

--- deploy-message-service.sh
+++ deploy-message-service.sh
@@ -10,7 +10,7 @@ docker pull federicofiorini/message-service:latest
 if docker stop message-service; then docker rm message-service; fi
 
 # Start message service container
-docker run --name message-service --restart=always \
+docker run -d --name message-service --restart=always \
     -e RABBITMQ_HOST=rabbit \
     -e RABBITMQ_USER=$RABBITMQ_USER \
     -e RABBITMQ_PASSWORD=$RABBITMQ_PASSWORD \
